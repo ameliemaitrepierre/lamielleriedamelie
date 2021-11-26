c6e1196802fee50d54bd93756ca1e1e1ce692218
Benjamin PERNEY <benjperney@gmail.com>
2021-11-26 11:54:33 +0100

    design landing page

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
     extend: {
       colors: {
         primary: '#FDDD64',
+        primary600: '#D6BA55'
+      },
+      borderRadius: {
+        xl: "30px"
+      },
+      boxShadow: {
+        cta: "-2px 2px 0px 2px #FEDD64",
+        ctaHover: "-4px 4px 0px 4px #FEDD64",
+        ctaBg: "-2px 2px 0px 2px #000000"
       },
       fontFamily: {
         sans: ['Josefin Sans', 'Arial', 'sans-serif'],
@@ -17,6 +26,9 @@ module.exports = {
       },
       gridTemplateColumns: {
         services: '150px 1fr'
+      },
+      minHeight: {
+        waves: '90px'
       }
     },
   },
