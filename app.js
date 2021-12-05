d32cdfd85922dc469d3902e70e2dcaf35bb32595
Benjamin PERNEY <benjamin.perney@interieur.gouv.fr>
2021-12-05 12:10:46 +0100

    WIP interaction observers

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
   // Create the observer
   const observer = new IntersectionObserver(entries => {
     // We will fill in the callback later...
+    console.log(entries)
+    entries.forEach(entry => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate-fade-in-down-0')
+      }
+    })
   });
 
   // Tell the observer which elements to track
-  observer.observe(document.querySelector('.square'));
+  observer.observe(document.querySelector('.js-interaction'));
 });
