42685890ac3c9543f67cccab7e92ca4eb746693d
Benjamin PERNEY <benjperney@gmail.com>
2022-02-19 14:51:04 +0100

    (style): suppression console log et commentaire

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  /* document.querySelector('.js-triggerAnimation').addEventListener('click', function() {
-    console.log('click button')
-    document.querySelector('.js-interaction').classList.add('animate-fade-in-down')
-  }) */
-  // Create the observer
   const observer = new IntersectionObserver(entries => {
-    // console.log(entries)
     entries.forEach(entry => {
-      console.log(entry)
       if (entry.isIntersecting) {
         entry.target.classList.add('animate-fade-in-down')
       }
     })
   });
 
-  // Tell the observer which elements to track
   const spies = document.querySelectorAll('.js-interaction')
   spies.forEach(spy => {
     observer.observe(spy);
@@ -24,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('menu-btn').addEventListener('click', (e) => {
     e.preventDefault()
     let target = e.target
-    console.log(this)
     if (target.classList.contains('open')) {
       target.classList.remove('open')
       target.classList.add('close')
